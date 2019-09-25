a3f631945510ed6c1414393b5000f79e2b53d755
Chris Jones <chris.jones@viget.com>
2019-09-25 16:40:47 -0400

    Fix Pantheon not finding web/wp directory

--- web/wp-config.php
+++ web/wp-config.php
@@ -1,10 +1,10 @@
 <?php
-
 /**
  * This config file is yours to hack on. It will work out of the box on Pantheon
  * but you may find there are a lot of neat tricks to be used here.
  *
  * See our documentation for more details:
+ *
  * https://pantheon.io/docs
  */
 
@@ -13,21 +13,21 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
 
     // Pantheon config
-    define('DB_NAME', $_ENV['DB_NAME']);
-    define('DB_USER', $_ENV['DB_USER']);
+    define('DB_NAME',     $_ENV['DB_NAME']);
+    define('DB_USER',     $_ENV['DB_USER']);
     define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
-    define('DB_HOST', $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT']);
-    define('DB_CHARSET', 'utf8');
-    define('DB_COLLATE', '');
+    define('DB_HOST',     $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT']);
+    define('DB_CHARSET',  'utf8');
+    define('DB_COLLATE',  '');
 
-    define('AUTH_KEY', $_ENV['AUTH_KEY']);
-    define('SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY']);
-    define('LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY']);
-    define('NONCE_KEY', $_ENV['NONCE_KEY']);
-    define('AUTH_SALT', $_ENV['AUTH_SALT']);
+    define('AUTH_KEY',         $_ENV['AUTH_KEY']);
+    define('SECURE_AUTH_KEY',  $_ENV['SECURE_AUTH_KEY']);
+    define('LOGGED_IN_KEY',    $_ENV['LOGGED_IN_KEY']);
+    define('NONCE_KEY',        $_ENV['NONCE_KEY']);
+    define('AUTH_SALT',        $_ENV['AUTH_SALT']);
     define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
-    define('LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT']);
-    define('NONCE_SALT', $_ENV['NONCE_SALT']);
+    define('LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT']);
+    define('NONCE_SALT',       $_ENV['NONCE_SALT']);
 
     // A couple extra tweaks to help things run well on Pantheon
     if (isset($_SERVER['HTTP_HOST'])) {
@@ -43,12 +43,15 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
             $scheme = 'https';
             $_SERVER['HTTPS'] = 'on';
         }
-
-        define('WP_HOME', $scheme . '://' . $_SERVER['HTTP_HOST']);
-        define('WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST']);
     }
 
-    // Don't show deprecations, useful for PHP < 5.5
+    define('WP_HOME',    $scheme . '://' . $_SERVER['HTTP_HOST']);
+    define('WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST'] . '/wp');
+
+    define('WP_CONTENT_DIR', dirname(__DIR__) . '/web');
+    define('WP_CONTENT_URL', WP_HOME);
+
+    // Don't show deprecations; useful under PHP 5.5
     error_reporting(E_ALL ^ E_DEPRECATED);
 
     // Define appropriate location for default tmp directory on Pantheon
@@ -79,30 +82,30 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
     define('WP_ENV', env('WP_ENV') ?: 'production');
 
     // Custom WordPress directory
-    define('WP_SITEURL', env('WP_SITEURL'));
-    define('WP_HOME', env('WP_HOME'));
+    define('WP_SITEURL',     env('WP_SITEURL'));
+    define('WP_HOME',        env('WP_HOME'));
     define('WP_CONTENT_DIR', dirname(__DIR__) . '/web');
     define('WP_CONTENT_URL', WP_HOME);
 
     // MySQL settings
-    define('DB_NAME', env('DB_NAME'));
-    define('DB_USER', env('DB_USER'));
+    define('DB_NAME',     env('DB_NAME'));
+    define('DB_USER',     env('DB_USER'));
     define('DB_PASSWORD', env('DB_PASSWORD'));
-    define('DB_HOST', env('DB_HOST') ?: 'localhost');
-    define('DB_CHARSET', 'utf8mb4');
-    define('DB_COLLATE', '');
+    define('DB_HOST',     env('DB_HOST') ?: 'localhost');
+    define('DB_CHARSET',  'utf8mb4');
+    define('DB_COLLATE',  '');
 
     $table_prefix = env('DB_PREFIX') ?: 'wp_';
 
     // Authentication unique keys and salts
-    define('AUTH_KEY', env('AUTH_KEY'));
-    define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
-    define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
-    define('NONCE_KEY', env('NONCE_KEY'));
-    define('AUTH_SALT', env('AUTH_SALT'));
+    define('AUTH_KEY',         env('AUTH_KEY'));
+    define('SECURE_AUTH_KEY',  env('SECURE_AUTH_KEY'));
+    define('LOGGED_IN_KEY',    env('LOGGED_IN_KEY'));
+    define('NONCE_KEY',        env('NONCE_KEY'));
+    define('AUTH_SALT',        env('AUTH_SALT'));
     define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
-    define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
-    define('NONCE_SALT', env('NONCE_SALT'));
+    define('LOGGED_IN_SALT',   env('LOGGED_IN_SALT'));
+    define('NONCE_SALT',       env('NONCE_SALT'));
 }
 
 // Standard wp-config.php stuff below

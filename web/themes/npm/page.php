e3e042773e12ce035716d99827d9879222f5d37f
Justin Vandenberg <justinjvandenberg@gmail.com>
2019-10-29 11:25:35 -0600

    Added text with image block

--- web/themes/npm/page.php
+++ web/themes/npm/page.php
@@ -3,7 +3,4 @@
 $context = Timber::get_context();
 $context['fields'] = get_fields();
 
-Timber::render(array(
-    $context['post']->post_name . '/index.twig',
-    'page.twig',
-), $context);
+Timber::render('templates/page/index.twig', $context);
---
+++ web/themes/npm/views/_blocks/text-with-image/fields.php
@@ -0,0 +1,95 @@
+<?php
+
+if (function_exists('acf_dynamic_clones_add_local_field_group')):
+
+    acf_dynamic_clones_add_local_field_group(array(
+        'key' => 'group_5db870452293c',
+        'title' => 'Text with Image',
+        'fields' => array(
+            array(
+                'key' => 'field_5db8710fa24e1',
+                'label' => 'Alignment',
+                'name' => 'alignment',
+                'type' => 'clone',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'clone' => array(
+                    0 => 'group_9nzyqkg26b6ok',
+                ),
+                'display' => 'seamless',
+                'layout' => 'block',
+                'prefix_label' => 1,
+                'prefix_name' => 1,
+            ),
+            array(
+                'key' => 'field_5db8709fa24df',
+                'label' => 'Image',
+                'name' => 'image',
+                'type' => 'image',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'array',
+                'preview_size' => 'thumbnail',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+            array(
+                'key' => 'field_5db8704aa24dd',
+                'label' => 'Heading',
+                'name' => 'heading',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5db87097a24de',
+                'label' => 'Text',
+                'name' => 'text',
+                'type' => 'wysiwyg',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'tabs' => 'all',
+                'toolbar' => 'basic',
+                'media_upload' => 0,
+                'delay' => 0,
+            ),
+        ),
+    ));
+
+endif;

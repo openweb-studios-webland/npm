c072080a840ec8a21c04fe61dff76f45f0f5e458
Justin Vandenberg <justinjvandenberg@gmail.com>
2019-11-19 17:03:44 -0700

    yarn build

---
+++ web/themes/npm/build/preload.bc31d30b5767761eccba.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{128:function(n,o,t){"use strict";t.r(o),t.d(o,"default",(function(){return a}));var a=function n(){!function(n,o){if(!(n instanceof o))throw new TypeError("Cannot call a class as a function")}(this,n),window.addEventListener("load",(function(){document.body.classList.remove("preload")}))}}}]);

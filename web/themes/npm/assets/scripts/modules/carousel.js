7a5ee77c9989d7a1c7d91749c0f1be63a056e263
Justin Vandenberg <justinjvandenberg@gmail.com>
2019-12-02 09:20:28 -0700

    Updated front page hero carousel

--- web/themes/npm/assets/scripts/modules/carousel.js
+++ web/themes/npm/assets/scripts/modules/carousel.js
@@ -15,7 +15,7 @@ export default class Carousel {
       itemsToMove: this.el.dataset.carouselMove || 1, // data-carousel-move
       loop: this.el.dataset.carouselLoop === 'false' ? false : true, // data-carousel-loop
       transition: this.el.dataset.carouselTransition === 'false' ? false : true, // data-carousel-transition
-      transitionSpeed: this.el.dataset.carouselTransitionSpeed || 400, // data-carousel-transition-speed
+      transitionSpeed: this.el.dataset.carouselTransitionSpeed || 500, // data-carousel-transition-speed
     }
 
     // Initialize if carousel contians more than 1 item

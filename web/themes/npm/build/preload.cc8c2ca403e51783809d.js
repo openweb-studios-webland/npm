461397a02b1921f1335deed9147a01b11c8a781b
Justin Vandenberg <justinjvandenberg@gmail.com>
2019-12-13 15:58:05 -0700

    Fixed header menu spacing

--- web/themes/npm/build/preload.cc8c2ca403e51783809d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{127:function(n,o,t){"use strict";t.r(o),t.d(o,"default",(function(){return a}));var a=function n(){!function(n,o){if(!(n instanceof o))throw new TypeError("Cannot call a class as a function")}(this,n),window.addEventListener("load",(function(){document.body.classList.remove("preload")}))}}}]);

7b28a2d9f0059136f4d5e73c6ebf86ddf90e062a
Justin Vandenberg <justinjvandenberg@gmail.com>
2019-12-19 11:04:09 -0700

    Redirect to product page from hash

--- web/themes/npm/assets/scripts/modules/product-hash.js
+++ web/themes/npm/assets/scripts/modules/product-hash.js
@@ -12,35 +12,14 @@ export default class ProductHash {
       return
     }
 
-    const el = this.el.querySelector(`[data-product-hash="${hash}"]`)
+    const product = this.el.querySelector(`[data-product-hash="${hash}"]`)
 
-    if (el) {
-      const siblings = this.getSiblings(el)
-
-      this.removeSiblings(siblings)
+    if (product) {
+      this.redirect(product.querySelector('[href]').href)
     }
   }
 
-  getSiblings = el => {
-    let siblings = []
-    let sibling = el.parentNode.firstChild
-
-    // Loop through siblings and push to an array
-    while (sibling) {
-      if (sibling.nodeType === 1 && sibling !== el) {
-        siblings.push(sibling)
-      }
-
-      sibling = sibling.nextSibling
-    }
-
-    return siblings
-  }
-
-  removeSiblings = siblings => {
-    // Loop through and remove siblings from DOM
-    siblings.forEach(sibling => {
-      sibling.remove()
-    })
+  redirect = url => {
+    window.location.replace(url)
   }
 }

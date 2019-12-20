76a8dec382e50faecae6acaca25c57a01eaf555d
Justin Vandenberg <justinjvandenberg@gmail.com>
2019-12-20 11:19:01 -0700

    Added images gallery to single product and work

--- web/themes/npm/views/templates/single-product/fields.php
+++ web/themes/npm/views/templates/single-product/fields.php
@@ -22,6 +22,7 @@ if (function_exists('acf_add_local_field_group')):
                 'layouts' => array(
                     acf_dynamic_clones_get_local_field_group('Hero', 'hero-single-product'),
                     acf_dynamic_clones_get_local_field_group('Images', 'images'),
+                    acf_dynamic_clones_get_local_field_group('Images Gallery', 'images-gallery'),
                     acf_dynamic_clones_get_local_field_group('Spec Groups', 'spec-groups'),
                     acf_dynamic_clones_get_local_field_group('Testimonials', 'testimonials'),
                     acf_dynamic_clones_get_local_field_group('Related Products', 'products'),
